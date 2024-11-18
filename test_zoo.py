224123c50d94d8c4b9cf77236ff53cef31b7ac71
Pongpon Butsrimart <wavenakab2019@gmail.com>
2024-11-19 01:11:20 +0700

    ✅ unit testing and fix  bugs

--- test_zoo.py
+++ test_zoo.py
@@ -5,8 +5,20 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_age_below_zero(self):
+        self.assertRaises(ValueError, self.zoo.get_ticket_price, -1)
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+
+    def test_senior_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
        
     # Add your additional test cases here.
 
